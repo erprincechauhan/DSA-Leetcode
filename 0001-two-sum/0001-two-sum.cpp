ccdd131e5e29b34a8ab1b301dd20dcb370d142fd
Prince Chauhan <erprincechauhan@gmail.com>
2026-08-07 13:46:11 +0530

    Time: 3 ms (66.31%), Space: 14.8 MB (57.27%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,15 +1,29 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        for (int i = 0; i < nums.size(); i++) {
+        // BRUTE
+        // for (int i = 0; i < nums.size(); i++) {
         
-            for (int j = i + 1; j < nums.size(); j++) {
+        //     for (int j = i + 1; j < nums.size(); j++) {
             
-                if (nums[i] + nums[j] == target) {
-                    return {i, j};
-                }
+        //         if (nums[i] + nums[j] == target) {
+        //             return {i, j};
+        //         }
+        //     }
+        // }
+
+        // Optimal
+        unordered_map<int,int> seen;
+
+        for(int i = 0 ; i < nums.size(); i++){
+            int complement = target - nums[i]; 
+
+            if(seen.find(complement) != seen.end()){
+                return {seen[complement],i};
             }
+            seen[nums[i]] = i;
         }
+
         return {};
     }
 };
